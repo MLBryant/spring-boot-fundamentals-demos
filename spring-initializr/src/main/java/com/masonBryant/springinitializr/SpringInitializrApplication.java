5ac2045a58610cacf08b528891e8c0301b252c90
Mason Bryant <mlbryant6@gmail.com>
2022-01-03 04:11:44 -0600

    added spring initializr greeting code

--- spring-initializr/src/main/java/com/masonBryant/springinitializr/SpringInitializrApplication.java
+++ spring-initializr/src/main/java/com/masonBryant/springinitializr/SpringInitializrApplication.java
@@ -8,6 +8,7 @@ public class SpringInitializrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringInitializrApplication.class, args);
+		System.out.println("Hello Mason Bryant!");
 	}
 
 }
